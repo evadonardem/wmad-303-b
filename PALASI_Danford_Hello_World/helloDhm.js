1f570b34fcc03a960ef67ce11c6957caa8a486fe
STARSZ1 <2022-01-0176@kcp.edu.ph>
2024-09-07 11:08:12 +0800

    message

--- PALASI_Danford_Hello_World/helloDhm.js
+++
@@ -1,7 +0,0 @@
-const pangalan = prompt("what is your name");
-const kulay = prompt("what is your favorite color");
-const likodkulay = prompt("what is your preferred background color");
-
-document.getElementById("nameDhm").innerHTML = pangalan;
-document.getElementById("nameDhm").style.color = kulay;
-document.body.style.backgroundColor = likodkulay;
