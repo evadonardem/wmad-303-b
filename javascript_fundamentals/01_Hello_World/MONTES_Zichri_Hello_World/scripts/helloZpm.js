672bac45c675dad0fda6b4d0c7dbf319471baafa
LGmonitor <xenonzero40@gmail.com>
2024-09-08 21:27:37 +0800

    02_Roll_the_dice

--- javascript_fundamentals/01_Hello_World/MONTES_Zichri_Hello_World/scripts/helloZpm.js
+++
@@ -1,7 +0,0 @@
-const pangalan = prompt("What is your name?");
-const kulay = prompt("What is your favorite color?");
-const likodkulay = prompt ("What is your preferred color?");
-
-document.getElementById("nameJaj").innerHTML = pangalan;
-document.getElementById("nameJaj").style.color = kulay;
-document.body.style.backgroundColor = likodkulay;
