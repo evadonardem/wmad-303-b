f53a202a2b3df5ed4d8493d05e79c8d5070e6ce5
Shane-tot <2023-01-1066@kcp.edu.ph>
2024-09-14 14:43:05 +0800

    Grade_Evaluator

---
+++ javascript_fundamentals/03_Grade_Evaluator/ULLANI_Ronalyn_Grade_Evaluator/scripts/grade_evaluator.js
@@ -0,0 +1,18 @@
+const card = document.getElementById("gradeCard");
+
+document.getElementById("yourGrade").onchange = (event) => {
+    const grade = event.target.value;
+
+    card.classList.remove("text-bg-success", "text-bg-danger");
+
+    if(grade >= 75){
+        card.getElementsByClassName("card-title")[0].innerText = "You've PASSED!";
+        card.classList.add("text-bg-success");
+    }else{
+        card.getElementsByClassName("card-title")[0].innerText = "Sorry! FAILED!";
+        card.classList.add("text-bg-danger");
+    }
+
+    document.getElementById("evaluation").innerText = evaluation;
+
+};
