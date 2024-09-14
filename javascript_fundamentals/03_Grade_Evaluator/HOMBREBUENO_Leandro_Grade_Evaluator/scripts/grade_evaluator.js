a322114584dc19cb3080b55b1bb1d563641429fc
Lean3029 <rihyon54@gmail.com>
2024-09-14 14:45:26 +0800

    hello

---
+++ javascript_fundamentals/03_Grade_Evaluator/HOMBREBUENO_Leandro_Grade_Evaluator/scripts/grade_evaluator.js
@@ -0,0 +1,21 @@
+
+const card = document.getElementById("gradeCard");
+
+
+
+document.getElementById("yourGrade").onchange = (event) => {
+    const grade = event.target.value;
+
+    card.classList.remove("text-bg-success", "text-bg-danger");
+
+    if(grade >= 75){
+        card.getElementsByClassName("card-title")[0].innerText = "Congratulations!";
+        card.classList.add("text-bg-success");
+    }else{
+        card.getElementsByClassName("card-title")[0].innerText = "FAILED!";
+        card.classList.add("text-bg-danger");
+    }
+
+    document.getElementById("evaluation").innerText = evaluation;
+    
+};
