eff6cc76aedeba894b6cc399497f4da349e5a2f5
ZymirDepaynos <147064336+ZymirDepaynos@users.noreply.github.com>
2024-09-30 13:03:06 +0800

    Add files via upload

---
+++ DEPAYNOS_Zymir_Grade_Evaluator/scripts/grade_evaluator.js
@@ -0,0 +1,20 @@
+const card = document.getElementById("gradeCard");
+
+document.getElementById("yourGrade").onchange = (event) => {
+    const grade = event.target.value;
+
+    card.classList.remove("text-bg-success", "text-bg-danger")
+
+    let evaluation;
+    if (grade >= 75){
+        card.classList.add("text-bg-success");
+        card.getElementsByClassName("card-title")[0].innerHTML = "You Passed"; 
+
+    } else {
+        card.classList.add("text-bg-danger");
+        card.getElementsByClassName("card-title")[0].innerHTML = "You Failed"; 
+
+    }
+
+    document.getElementById("evaluation").innerHTML = evaluation;
+}
