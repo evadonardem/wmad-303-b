80ef2df69f41505cdbd9ba4585522bbb5908794e
itsolyn <lynitso1234@gmail.com>
2024-12-12 04:35:57 -0600

    Update +not-found.tsx

--- ITSO_Lyn_My_First_App/app/+not-found.tsx
+++ ITSO_Lyn_My_First_App/app/+not-found.tsx
@@ -1,7 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
-export default function NotFoundScreennfhd() {
+export default function NotFoundScreenlx() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
