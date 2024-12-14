740808834f13b0116119f61dca5cbb97ad539489
Renren1223 <deverarenand21@gmail.com>
2024-12-15 01:33:34 +0800

    Hello world 2

---
+++ scripts/helloRmpa.js
@@ -0,0 +1,8 @@
+
+const pangalan = prompt("What is your name?"); 
+const kulay = prompt("What is your favorite color?");
+const likodkulay = prompt("What is your prefered background color?");
+
+document.getElementById("nameRmpa").innerHTML = pangalan;
+document.getElementById("nameRmpa").style.color = kulay;
+document.body.style.backgroundColor = likodkulay;
